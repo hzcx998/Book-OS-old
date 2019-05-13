c7215c3d1412d6c8786c9662a6366f15f2894f4c
Hu Zicheng <2323168280@qq.com>
2019-05-14 02:08:36 +0800

    New disk

--- src/include/sys/const.h
+++ src/include/sys/const.h
@@ -6,11 +6,11 @@ Version: -v0.1 2018/9/26~2019/1/7
 Version: -v0.2 2019/1/8~2019/2/21
 Version: -v0.3 2019/2/22~2019/4/6
 Version: -v0.39 2019/4/7~2019/5/2
-
+Version: -v0.4 2019/5/3~2019/5/13
 */
 
 #define OS_NAME "Book OS"
-#define OS_VERSION "0.39"
+#define OS_VERSION "0.4"
 #define OS_COPYRIGHT "(C) 2018-2019 by Book OS developers. All rights reserved."
 #define OS_AUTHER "Hu Zicheng"
 
