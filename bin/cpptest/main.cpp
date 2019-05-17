a1ed06e5884041fc2c721973ef4bb044c21f2b63
dorbmon <1263592223@qq.com>
2019-05-17 13:08:27 +0800

    add part of iostream

--- bin/cpptest/main.cpp
+++ bin/cpptest/main.cpp
@@ -1,27 +1,12 @@
 extern "C" {
 	#include <stdio.h>
-	#include <stdlib.h>
 }
-#include <new>
 #include <vector>
 #include <string>
+#include <iostream>
 using namespace std;
 int main(int argc, char *argv0[]) {
-	printf("test\n");
-	vector<int> data;
-	data.push_back(0);
-	if(data.empty()) {
-		printf("empty");
-	}else {
-		printf("not empty");
-	}
-	for(int i = 0;i < 10;i ++) {
-		data.push_back(i);
-	}
-	printf("pushed \n");
-	for(int b = 0;b < 1;b ++) {
-		printf("%d \n",data.pop_back());
-	}
-	
+	string test = "2333";
+	cout << test;
 	return 0;
 }
